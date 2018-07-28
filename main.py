81a806b5141d595ac0877551cc4d39496a4f96bd
osiykm <osiykmukola@gmail.com>
2018-07-28 05:00:20 +0300

    fix ':' in download

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ import my_parser
 if __name__ == '__main__':
     urls = open(URLS_FILE, 'r').readlines()
     for url in urls:
-        book = my_parser.parse(url=str(url))
-        create_epub(SAVE_FOLDER + '/' + book.name.replace(" ", "_") + ".epub", book)
+        try:
+            book = my_parser.parse(url=str(url))
+            print('create book ', book.name.replace(" ", "_").replace(":", ""), ".epub")
+            create_epub(SAVE_FOLDER + '/' + book.name.replace(" ", "_").replace(":", "") + ".epub", book)
+        except ValueError:
+            print("Not Found")
